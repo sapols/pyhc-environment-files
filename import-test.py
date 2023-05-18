d2726c70c09300904a4be592cfc096caa2ae09a8
Shawn Polson <sapols@yahoo.com>
2023-05-18 14:30:53 -0600

    Add files via upload

---
+++ import-test.py
@@ -0,0 +1,53 @@
+import aacgmv2
+import aiapy
+import aidapy
+import apexpy
+import astrometry_azel
+import cdflib
+import dascutils
+import dbprocessing
+import dmsp
+import enlilviz
+import fiasco
+import georinex
+import geospacelab
+import goesutils
+import hissw
+import igrf
+import iri2016
+# import irispy_lmsal
+import lowtran
+import mcalf
+import msise00
+import ndcube
+import nexradutils
+# import ocbpy
+import OMMBV
+import pydarn
+import pyflct
+import pytplot  # should pull in pytplot_mpl_temp
+import pyzenodo3
+import reesaurora
+import sciencedates
+import solarmach
+import solo_epd_loader
+import speasy
+import spiceypy
+import sunkit_image
+import sunkit_instruments
+import sunraster
+import themisasi
+import viresclient
+# import wmm2015
+# import wmm2020
+import hapiclient
+import kamodo
+import plasmapy
+import pysat
+# import pyspedas
+import spacepy
+import sunpy
+
+
+print("\nAll the PyHC imports worked!\n")
+
