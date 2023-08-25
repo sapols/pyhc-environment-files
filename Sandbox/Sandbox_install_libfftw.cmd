bd9f6bcaed51bac242b5bb65fa9149a38276ec5c
Alexander Drozdov (Sasha) <adrozdov@ucla.edu>
2023-08-25 15:55:13 -0700

    installation of libfftw fixed

--- Sandbox/Sandbox_install_libfftw.cmd
+++ Sandbox/Sandbox_install_libfftw.cmd
@@ -7,10 +7,10 @@ mkdir %libfftw3path%
 tar -xf %downloadpath%fftw-3.3.5-dll64.zip -C %libfftw3path%
 
 REM Install library
-%comspec% "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat" & ^
+start /wait %comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat & ^
 lib /def:libfftw3-3.def /machine:x64 & ^
 lib /def:libfftw3f-3.def /machine:x64 & ^
-lib /def:libfftw3l-3.def /machine:x64
+lib /def:libfftw3l-3.def /machine:x64 & exit"
 
 REM Add to path
 setx /M path "%path%;%libfftw3path%"
