4308caa524509a8450d6744ca475fd8c73ab0678
Alexander Drozdov (Sasha) <adrozdov@ucla.edu>
2023-08-26 12:05:38 -0700

    Fixed installation of C++ build tools

--- Sandbox/Sandbox_install_requrements.cmd
+++ Sandbox/Sandbox_install_requrements.cmd
@@ -5,8 +5,11 @@ set scriptpath=C:\users\WDAGUtilityAccount\Desktop\pyhc-environment-files\Sandbo
 REM Install minial requrements
 call %scriptpath%Sandbox_install_minimal_requrements.cmd
 
+REM Update enviroment
+call %scriptpath%RefreshEnv.cmd
+
 REM Install "Visual Studio 2022 Build Tools"
-%installpath%vs_buildtools.exe --quiet
+%installpath%vs_buildtools.exe --passive --includeRecommended --add Microsoft.VisualStudio.Product.BuildTools --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.Microsoft.VisualStudio.Component.Windows10SDK.20348 --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64
 
 REM Install the rest of the packages
 call %scriptpath%Sandbox_install_fortran_via_msys2.cmd
