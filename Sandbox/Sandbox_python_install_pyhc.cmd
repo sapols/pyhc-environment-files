d1971f0f7dce05d62fea0597a9f9626c2e24592c
Alexander Drozdov (Sasha) <adrozdov@ucla.edu>
2023-08-26 12:06:33 -0700

    Update pip

--- Sandbox/Sandbox_python_install_pyhc.cmd
+++ Sandbox/Sandbox_python_install_pyhc.cmd
@@ -1,4 +1,5 @@
 call C:\venv\pyhc\Scripts\activate.bat
 cd C:\Users\WDAGUtilityAccount\Desktop\pyhc-environment-files
+python.exe -m pip install --upgrade pip
 pip install -r pyhc-requirements.txt
 pip install -r pyhc-packages-requirements.txt
